438bc631e5ba4e85c50ee769f5fad9182271c45e
Anže Oblak <anze.oblak@rettro.si>
2023-01-11 22:53:52 +0100

    shopping cart price compare mock

--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -1,8 +1,28 @@
 <template>
   <h4 v-if="cartId === 'favourites'" class="text-start ms-3 mb-2 mt-4">Priljubljeni izdelki</h4>
   <h4 v-else class="text-start ms-3 mb-2 mt-4">Izdelki v košarici</h4>
-  <b-table striped :items="products" :fields="productsFields" show-empty>
+  <b-table striped :items="products" :fields="productsFields" show-empty responsive="sm">
     <template #empty>V košarici še ni produktov</template>
+    <template #cell(show_details)="row">
+      <vue-feather type="chevron-down" @click="row.toggleDetails" class="mr-2 align-middle" style="cursor: pointer">
+      </vue-feather>
+    </template>
+
+    <template #row-details="row">
+      <b-card>
+        <b-row class="mb-2">
+          <b-col class="text-sm-right"><b>Trgovina</b></b-col>
+          <b-col><b>Cena</b></b-col>
+          <b-col v-if="cartId !== 'favourites'"><b>Cena za količino</b></b-col>
+        </b-row>
+        <b-row v-for="store in computedStores" :key="store">
+          <b-col class="text-sm-right">{{ store.name }}</b-col>
+          <b-col>{{ store.price }}€</b-col>
+          <b-col v-if="cartId !== 'favourites'">{{ store.price * row.item.quantity }}€</b-col>
+        </b-row>
+
+      </b-card>
+    </template>
   </b-table>
 
   <b-button @click="$router.push('/')">Nazaj</b-button>
@@ -10,7 +30,7 @@
 
 <script>
 import app from "@/main";
-import {SHOPPING_CART_URL, PRODUCT_CATALOG_URL} from "@/utils/config";
+import {SHOPPING_CART_URL, PRODUCT_CATALOG_URL, STORE_CATALOG_URL} from "@/utils/config";
 
 export default {
   name: "ShoppingCart",
@@ -21,23 +41,35 @@ export default {
       products: [],
       productsFields: [
         {
+          key: 'show_details',
+          label: '',
+          thStyle: 'width: 5%'
+        },
+        {
           key: 'name',
           label: 'Naziv',
-          thStyle: 'width: 30%'
+          thStyle: 'width: 60%'
         },
         {
           key: 'quantity',
           label: 'Količina'
         }
       ],
+      stores: []
     }
   },
 
   mounted() {
-    console.log(this.$router.currentRoute.value.params.id)
     if (this.cartId === 'favourites')
       this.productsFields = this.productsFields.filter(item => item.key !== 'quantity')
     this.getProducts();
+    this.getStores()
+  },
+
+  computed: {
+    computedStores() {
+      return this.stores.map(item => ({...item, price: Math.floor(Math.random() * (100 - 5) + 5)}))
+    }
   },
 
   methods: {
@@ -50,17 +82,38 @@ export default {
             .catch(err => {
               console.error(err)
             })
-      }
-      else {
+      } else {
         app.axios.get(`${SHOPPING_CART_URL}/shopping-cart/${this.cartId}`)
+            .then(resp => {
+              this.products = resp.data.products;
+            })
+            .catch(err => {
+              console.error(err)
+            })
+      }
+    },
+
+    getStores() {
+      let graphQLQuery = `query MyQuery {
+        allStores {
+          result {
+            id
+            name
+            url
+          }
+        }
+      }`
+
+      app.axios.post(`${STORE_CATALOG_URL.replaceAll('/v1', '/')}graphql`, {query: graphQLQuery}, {
+        headers: {'Content-Type': 'application/json'}
+      })
           .then(resp => {
-            this.products = resp.data.products;
+            this.stores = resp.data.data.allStores.result
           })
           .catch(err => {
             console.error(err)
           })
-      }
-    }
+    },
   }
 }
 </script>
